4ca4a88b22b75974990189a5504966fa42dc03c6
ZOO1E <73783859+ZOO1E@users.noreply.github.com>
2020-12-25 23:34:19 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ const diceEl =document.querySelector('.dice');
 const btnnew =document.querySelector('.btn--new');
 const btnRoll =document.querySelector('.btn--roll');
 const btnHold =document.querySelector('.btn--hold');
-const current0El = document.querySelector('.#current--0');
-const current1El = document.querySelector('.#current--1');
-const player0 =document.querySelector('player player--0');
-const player1 =document.querySelector('player player--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const player0El =document.querySelector('.player--0');
+const player1El =document.querySelector('.player--1');
 
 
-
+const scores = [0,0];
 score1.textContent = 0;
 score0.textContent = 0;
 diceEl.classList.add('hidden');
 let currentScore =0;
-
+let activePlayer = 0;
+console.log(currentScore);
 
 //Rollinmg dice functionality
 btnRoll.addEventListener('click',function(){
@@ -26,20 +27,18 @@ btnRoll.addEventListener('click',function(){
 const dice =  Math.trunc(Math.random() * 6)+1
     //2. Display dice
 diceEl.classList.remove('hidden');
-diceEl.src = 'dice-'+ dice+'.png'
+diceEl.src = 'dice-'+ dice+'.png'; //how to bring img 
     //3.Check for rolled 1: if true , switch to next to player
 if(dice !== 1){
 //Add dice to the current score 
 currentScore += dice;
-current0El.textContent = currentScore;
+document.getElementById('current--'+ activePlayer).textContent =currentScore;
 }else{
 //Switch player
-if(player1){
-player0.classList.remove('player--activate');
-player1.classList.add('player--activate');
-}}
-if(player0){
-    player1.classList.remove('player--activate');
-    player0.classList.add('player--activate');
-    }
-})
+document.getElementById('current--'+ activePlayer).textContent = 0;
+currentScore = 0;
+activePlayer = activePlayer === 0 ? 1 : 0;// exactly need to understand 
+player0El.classList.toggle('player--active');//if statement is 'remove' -> 'add' , add-> remove
+player1El.classList.toggle('player--active');
+}
+});
